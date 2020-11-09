c90e8fbc0117e0f662e3dd163579089edd9e2efb
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-11-09 12:52:29 +0530

    Added : Singlet Combinatio:

--- coin.sh
+++ coin.sh
@@ -1,5 +1,9 @@
 echo "Welcome"
 echo "Coin flip simulator"
+declare -A Coin
+count=1
+hcount=0
+tcount=0
 randomValue=$(($RANDOM%2+1));
 if [ $randomValue == 1 ]
 then
@@ -7,3 +11,27 @@ then
 else
         echo "Tail";
 fi
+for((i=1;i<22;i++))
+do
+	randomValue=$(($RANDOM%2+1));
+	if [ $randomValue == 1 ]
+	then
+        	Coin[$i]='H'
+	else
+        	Coin[$i]='T'
+fi
+done
+
+for key in "${!Coin[@]}"
+do
+	if [[ ${Coin[$key]} == 'H' ]]
+        then
+                ((hCount++))
+        else
+                ((tCount++))
+        fi
+done
+hper=$(( $hCount*100/20 ))
+tper=$(( $tCount*100/20 ))
+echo "Percentage of Head is $hper"
+echo "Percentage of Tail is $tper"
